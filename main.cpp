d17e177359cc37c6cc447bfd3f8d74f220e4cfd8
Henrique Barucco <henrique_barucco@hotmail.com>
2024-09-30 21:14:10 -0300

    feat: console menu screen

--- main.cpp
+++ main.cpp
@@ -1,11 +1,72 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
 #include "../include/headers/ArpService.hpp"
 #include "../include/headers/UiService.hpp"
 
 using namespace std;
 
+void clearScreen() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+void showMenu() {
+    cout << "----- ARP Service Menu -----" << endl;
+    cout << "1. Show ARP Table" << endl;
+    cout << "2. Exit" << endl;
+    cout << "----------------------------" << endl;
+    cout << "Enter your choice: ";
+}
+
+void displayArpTable(const string& arpResult) {
+    cout << "----- ARP Table -----" << endl;
+    cout << "IP Address\t\tMAC Address\t\tInterface" << endl;
+    cout << "----------------------------------------------" << endl;
+
+    stringstream ss(arpResult);
+    string line;
+    while (getline(ss, line)) {
+        cout << line << endl;
+    }
+    cout << "-----------------------------" << endl;
+}
+
 int main() {
-    drawUi();
+    int choice;
+    bool running = true;
+    
+    while (running) {
+        clearScreen();
+        showMenu();
+        cin >> choice;
+
+        clearScreen();
+
+        switch (choice) {
+            case 1: {
+                string arpResults = executeArp();
+                displayArpTable(arpResults);
+                cout << "\nPress enter to return to menu...";
+                cin.ignore();
+                cin.get();
+                break;
+            }
+            case 2:
+                running = false;
+                break;
+            default:
+                cout << "Invalid choice. Please select again." << endl;
+                cout << "Press enter to return to menu...";
+                cin.ignore();
+                cin.get();
+        }
+    }
+
+    cout << "Exiting..." << endl;
     return 0;
 }
